0a46e6ed017035eb4429a1fbc06d0c9dd1d4d814
Diogo da Silva Santana <diogo.santana@MacBook-DGRTG.local>
2019-11-21 21:37:28 -0300

    Módulo 3

--- banco-de-dados/projeto/testes/insert.js
+++ banco-de-dados/projeto/testes/insert.js
@@ -1,24 +1,24 @@
 const db = require('../config/db')
 
-// const novoPerfil = {
-//   nome: 'visiante',
-//   rotulo: 'Visitante'
-// }
-
-// db('perfis').insert(novoPerfil)
-//   .then(res => console.log(res))
-//   .catch(error => console.log(error.sqlMessage))
-//   .finally(() => db.destroy())
-
-const perfilSU = {
-  nome: 'root' + Math.random(),
-  rotulo: 'Super Usuário'
+const novoPerfil = {
+  nome: 'visiante',
+  rotulo: 'Visitante'
 }
 
-// INSERT INTO PERFILS (nome, rotulo) VALUES ('...', '...')
-db.insert(perfilSU).into('perfis')
-  .then(res => res[0])
-  .then(id => `O código gerado foi ${id}`)
-  .then(string => console.log(string))
+db('perfis').insert(novoPerfil)
+  .then(res => console.log(res))
   .catch(error => console.log(error.sqlMessage))
   .finally(() => db.destroy())
+
+// const perfilSU = {
+//   nome: 'root' + Math.random(),
+//   rotulo: 'Super Usuário'
+// }
+
+// // INSERT INTO PERFILS (nome, rotulo) VALUES ('...', '...')
+// db.insert(perfilSU).into('perfis')
+//   .then(res => res[0])
+//   .then(id => `O código gerado foi ${id}`)
+//   .then(string => console.log(string))
+//   .catch(error => console.log(error.sqlMessage))
+//   .finally(() => db.destroy())
